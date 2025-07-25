9bda71cd3184a3624ce742a2f8f8355901e8d211
no2ca <taiseiono6@gmail.com>
2025-07-25 15:31:51 +0900

    Add the `-z noexecstack` option to script

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 cargo run "$1" > tmp.s
-gcc -o tmp tmp.s
+gcc -z noexecstack -o tmp tmp.s
 ./tmp
 echo "OUTPUT: $?"
 rm -f tmp tmp.s
--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@ assert() {
     expected="$1"
     input="$2"
     cargo run -r "$input" > tmp.s
-    gcc -o tmp tmp.s
+    gcc -z noexecstack -o tmp tmp.s
     ./tmp
     actual="$?"
     if [ "$actual" = "$expected" ]; then 
