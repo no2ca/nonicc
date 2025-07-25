b5b3b997edaa06a62b5cd6c982063f13c8b416cc
no2ca <taiseiono6@gmail.com>
2025-07-25 23:12:25 +0900

    Add a semicolon to a test case

--- test.sh
+++ test.sh
@@ -14,26 +14,26 @@ assert() {
     fi
 }
 
-assert 0 0
+assert 0 '0;'
 
-assert 2 1+1
+assert 2 '1+1;'
 
-assert 42 ' 25 - 2 + 19 '
-assert 21 ' 42 / 2 '
+assert 42 ' 25 - 2 + 19; '
+assert 21 ' 42 / 2; '
 
-assert 2 ' 1 + (2 + 3) / 4 '
-assert 6 '(1) / 2 + (2 * 3)'
+assert 2 ' 1 + (2 + 3) / 4; '
+assert 6 '(1) / 2 + (2 * 3); '
 
-assert 0 ' +1 + -1 '
-assert 0 ' (-1) - -1 '
-assert 1 ' (-1) * -1 '
+assert 0 ' +1 + -1; '
+assert 0 ' (-1) - -1; '
+assert 1 ' (-1) * -1; '
 
-assert 0 ' 1 != 1 '
-assert 1 ' 1 == 1 '
-assert 0 ' 1 < 1 '
-assert 1 ' 1 <= 1 '
-assert 0 ' 1 > 1 '
-assert 1 ' 1 >= 1 '
+assert 0 ' 1 != 1; '
+assert 1 ' 1 == 1; '
+assert 0 ' 1 < 1; '
+assert 1 ' 1 <= 1; '
+assert 0 ' 1 > 1; '
+assert 1 ' 1 >= 1; '
 
 rm -f tmp*
 
